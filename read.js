f619019b3a55d0c7fc315d0c8d65425d4176321d
shbahi <m.shbahi200@gmail.com>
2025-01-10 21:41:37 +0300

    ReadFile

---
+++ read.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  fs.readFile("info.txt", (err, data) => {
+    if (err) {
+      res.writeHead(500, { "content-type": "text/plain" });
+      console.log("sorry can't read from the server");
+    } else {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.end(data);
+    }
+  });
+});
+
+const thePort = 3000;
+
+const HOST = "127.0.0.1";
+
+server.listen(thePort, HOST, () => {
+  console.log(
+    `the server is now running on port # ${thePort} and host ${HOST}`
+  );
+});
